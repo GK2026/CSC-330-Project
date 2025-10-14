03e87c6b454794f9e4f68ceb11c327f699a31ea7
kirbyg2@southernct.edu <kirbyg2@southernct.edu>
2025-10-14 15:58:31 +0000

    Working Websit

--- Prototype.js
+++ Prototype.js
@@ -1,7 +1,19 @@
-const message = 'This is our prototype' // Try edit me
+const http = require('http'); // Import the HTTP module
 
-// Update header text
-document.querySelector('#header').innerHTML = message
+// Create a server object
+const myserver = http.createServer(function (req, res) {
 
-// Log to console
-console.log(message)
+  // Set the response header with a status code and content type
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+
+  // Write a response message to the client
+  res.write('Hello World!');
+
+  // End the response
+  res.end();
+});
+
+// Make the server listen on port 80
+myserver.listen(80, () => {
+  console.log('Server is running on port 80...');
+});
