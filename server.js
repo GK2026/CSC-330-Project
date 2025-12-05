9e481418056c88258d006d18411533f8395579fc
Maia <lins4@southernct.edu>
2025-12-05 18:01:50 -0500

    working on signup

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ const server = http.createServer((req, res) => {
       const password = data.password;
 
       connection_pool.query(
-        "SELECT id FROM users WHERE username=?",
+        "SELECT username FROM users WHERE username=?",
         [username],
         function (err, results) { // retrieving mySQL's response to determine what to do with the data
           if (err) {
