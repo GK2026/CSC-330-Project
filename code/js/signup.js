8b973a5c018b88b73850025c1ea527493af0a72f
Maia <lins4@southernct.edu>
2025-12-05 15:44:55 -0500

    Working on sign up functionality and deleted redundant questionnaire.html files

--- code/js/signup.js
+++ code/js/signup.js
@@ -1,4 +1,22 @@
-let user = document.getElementById('userSignUp');
-let password = document.getElementById('passSignUp');
+document.getElementById("questDirect").addEventListener("click", async () => {
+  const data = {
+    username: document.getElementById("usernameEntry").value,
+    password: document.getElementById("passwordEntry").value,
+  };
 
-// ALL SQL CODE MOVED TO server.js, they should be backend, not here
+  try {
+    const res = await fetch("/signup", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data)
+    });
+
+    if (res.ok) {
+      alert("Your information has been saved!");
+    } else {
+      alert("There was an error saving your information.");
+    }
+  } catch (err) {
+    alert("Network error: could not send data.");
+  }
+});
